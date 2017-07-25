d4afdb9b055f3446f4de3ba75e96cad6254650c3
stepa <stepangrig677@gmail.com>
2017-07-25 11:33:08 +0300

    3

--- Calculator/Controllers/HomeController.cs
+++ Calculator/Controllers/HomeController.cs
@@ -4,11 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Calculator.Models;
+using Calculator.CalcultorDb;
+
 
 namespace Calculator.Controllers
 {
     public class HomeController : Controller
     {
+        private CalclatorDbContext DB = new CalclatorDbContext();
         [HttpGet]
         public ActionResult Index()
         {
@@ -18,12 +21,93 @@ namespace Calculator.Controllers
         }
 
         [HttpPost]
-
         public ActionResult Index(ResultModels model)
         {
-            ViewBag.Title = "Калькулятор";
+            var ip = System.Web.HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
 
-            return View(model);
+            if (System.Web.HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"] != null)
+            {
+                ip = System.Web.HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"].ToString();
+            }
+            else if (System.Web.HttpContext.Current.Request.UserHostAddress.Length != 0)
+            {
+                ip = System.Web.HttpContext.Current.Request.UserHostAddress;
+            }
+
+
+            ViewBag.Title = "Калькулятор";
+            Log log = new Log
+            {
+                ID = 1,
+                OperationTime = System.DateTime.Now,
+                IpAddress = ip,
+                Operands = model.Operations
+            };
+            DB.Logs.Add(log);
+            DB.SaveChanges();
+            try
+            {
+                model.Operations = Caclulate(model.Operations).ToString();
+                //DB.SaveChanges();
+                return View(model);
+            }
+            catch
+            {
+                //DB.SaveChanges();
+                return View("Error");
+            }
+
+            //log.Result = model.Operations;
+            
         }
+
+        #region Helpers
+
+        private static decimal Operation(decimal A, decimal B, char Operand)
+        {
+            decimal result;
+            switch (Operand)
+            {
+                case '+':
+                    result = A + B;
+                    break;
+                case '-':
+                    result = A - B;
+                    break;
+                case '*':
+                    result = A * B;
+                    break;
+                case '/':
+                    result = A / B;
+                    break;
+                default:
+                    result = A + B;
+                    break;
+            }
+            return result;
+        }
+        private static decimal Caclulate(string input)
+        {
+            decimal result;
+            decimal previos = 0;
+            string current = "";
+            char operand = '+';
+            foreach (char x in input)
+            {
+                if (x == '+' || x == '-' || x == '*' || x == '/')
+                {
+                    previos = Operation(previos, Decimal.Parse(current), operand);
+                    operand = x;
+                    current = "";
+                }
+                else
+                {
+                    current += x;
+                }
+            }
+            result = Operation(previos, Decimal.Parse(current), operand);
+            return result;
+        }
+        #endregion
     }
 }
